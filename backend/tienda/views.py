596882491e81debb8596fbb1cbde6aa6e371170b
Berenice Solohaga <bsolohaga@hep.local>
2025-07-22 11:51:02 -0300

    1

--- backend/tienda/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Product
-from .serializers import ProductoSerializer
-
-class ProductoViewSet(viewsets.ModelViewSet):
-        queryset = Product.objects.all()
-        serializer_class = ProductoSerializer
-        
--- backend/urls.py
+++ backend/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Bienvenido a la API del eCommerce")
+from django.urls import path
 
 urlpatterns = [
-    path('', home),  # Agregado para la raíz
     path('admin/', admin.site.urls),
-    path('api/', include('tienda.urls')),
 ]
