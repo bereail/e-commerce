32ac0236605c468c0349d6b0e434c61761648043
Berenice Solohaga <bsolohaga@hep.local>
2025-10-08 11:55:28 -0300

    full

---
+++ web/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Hero from "./Hero/page";
+import Categories from "./Categories/page";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Categories />
+    </>
+  );
+}
